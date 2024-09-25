738243b5fea1aaeb9e36c80af9d465b0025407fa
oscar <sogunder@gmail.com>
2024-09-25 10:20:05 -0300

    implementacion ev context

---
+++ EvaluacionProgramacion/Data/EvContext.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using EvaluacionProgramacion.Models;
+using System.Threading;
+
+namespace EvaluacionProgramacion.Services.Users.Data
+{
+    public class EvContext : DbContext
+    {
+        public EvContext(DbContextOptions<EvContext> options) : base(options)
+        {
+        }
+
+        /* DbSet indica el modelo que se va a mapear (reflejar) a la base de datos */
+        public DbSet<Rol> Roles { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Tarea> Tareas { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Acá se pueden cargar los datos iniciales de la base de datos
+
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                Id = 1,
+                Nombre = "Administrador"
+            });
+
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                Id = 2,
+                Nombre = "Empleado"
+            });
+
+        }
+
+    }
+
+}
