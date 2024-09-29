023b0c1bb05ded942556deadeb56835e770c7f95
Moka <ymunoz23@cftsa.cl>
2024-09-28 22:53:04 -0300

    Correcion y alimentacion de modelo Proyecto

--- EvaluacionProgramacion/Models/Proyecto.cs
+++ EvaluacionProgramacion/Models/Proyecto.cs
@@ -5,8 +5,12 @@ namespace EvaluacionProgramacion.Models
     public class Proyecto
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
+        public string Nombre{ get; set; }
         public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public int HorasTrabajadas { get; set; }
+        public int HorasTotales { get; set; }
+        public DateTime FechaCreacion { get; set; }
 
         public ICollection<Tarea> Tareas { get; set; }
     }
