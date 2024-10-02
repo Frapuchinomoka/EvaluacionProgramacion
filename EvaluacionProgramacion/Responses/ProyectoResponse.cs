a3e8aa70ffca94ed43719a9317416ea4a6f0a77c
Moka <ymunoz23@cftsa.cl>
2024-10-01 23:31:47 -0300

    Creacion de ProyectoResponse

---
+++ EvaluacionProgramacion/Responses/ProyectoResponse.cs
@@ -0,0 +1,30 @@
+﻿using EvaluacionProgramacion.Models;
+
+namespace EvaluacionProgramacion.Responses
+  
+    {
+        public class ProyectoResponse: ResponseBase<Proyecto>
+        {
+
+        }
+      
+        public class ProyectosResponse : ResponseBase<List<Proyecto>>
+        {
+
+        }
+       
+        public class NuevoProyectoResponse : ResponseBase<bool>
+        {
+
+        }
+        
+        public class UpdateProyectoResponse : ResponseBase<bool>
+        {
+
+        }
+        
+        public class DeleteProyectoResponse : ResponseBase<bool>
+        {
+
+        }
+    }
