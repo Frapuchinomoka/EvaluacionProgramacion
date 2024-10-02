6348e741f44c982247063f100e77a20ec4d27abf
Moka <ymunoz23@cftsa.cl>
2024-10-01 23:27:45 -0300

    Creacion de UsuarioResponse

---
+++ EvaluacionProgramacion/Responses/UsuarioResponse.cs
@@ -0,0 +1,32 @@
+﻿using EvaluacionProgramacion.Models;
+
+namespace EvaluacionProgramacion.Responses
+    {
+        public class UsuarioResponse : ResponseBase<Usuario>
+        {
+
+        }
+
+        public class UsuariosResponse : ResponseBase<List<Usuario>>
+        {
+
+        }
+
+        public class NuevoUsuarioResponse : ResponseBase<bool>
+        {
+
+        }
+
+ 
+        public class UpdateUsuarioResponse : ResponseBase<bool>
+        {
+
+        }
+
+     
+        public class DeleteUsuarioResponse : ResponseBase<bool>
+        {
+
+        }
+    }
+
