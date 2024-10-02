f5315595007190ad0a462e190099467de3175e66
Moka <ymunoz23@cftsa.cl>
2024-10-01 23:21:51 -0300

    Creacion de TareaResponse

---
+++ EvaluacionProgramacion/Responses/TareaResponse.cs
@@ -0,0 +1,33 @@
+﻿using EvaluacionProgramacion.Models;
+
+namespace EvaluacionProgramacion.Responses
+{
+
+    public class TareaResponse : ResponseBase<Tarea>
+        {
+
+        }
+
+        public class TareasResponse : ResponseBase<List<Tarea>>
+        {
+
+        }
+
+        public class NuevaTareaResponse : ResponseBase<bool>
+        {
+
+        }
+
+        public class UpdateTareaResponse : ResponseBase<bool>
+        {
+
+        }
+
+        public class DeleteTareaResponse : ResponseBase<bool>
+        {
+
+        }
+
+    }
+
+
